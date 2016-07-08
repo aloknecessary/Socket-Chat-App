2e65ec038f50ec712ed1cf51044d737bfbac65bb
Alok Ranjan <aloks@byteridge.com>
2016-07-08 16:53:52 +0530

    added bootstrap css and added welcome msg after register

--- Socket.io-Chat-App/Socket.io-Chat-App/server.js
+++ Socket.io-Chat-App/Socket.io-Chat-App/server.js
@@ -19,6 +19,22 @@ io.on('connection', function (socket) {
     //socket.on('chat message', function (msg) {
     //    socket.broadcast.emit('chat message', msg);
     //});
+    // when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function (username) {
+        // store the username in the socket session for this client
+        socket.username = username;
+        // store the room name in the socket session for this client
+        //socket.room = 'room1';
+        // add the client's username to the global list
+        //usernames[username] = username;
+        // send client to room 1
+        //socket.join('room1');
+        // echo to client they've connected
+        socket.emit('updatechat', 'Welcome ' + username);
+        // echo to room 1 that a person has connected to their room
+        //socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        //socket.emit('updaterooms', rooms, 'room1');
+    });
     //
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
